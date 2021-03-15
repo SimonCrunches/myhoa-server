09c9a3fe20244fa85a2c24ea55304e589e4ebd53
Simon Safonov <mrsandman138@gmail.com>
2021-03-15 16:07:11 +0300

    auth request fix

--- src/main/java/org/technopolis/configuration/security/WebSecurityConfig.java
+++ src/main/java/org/technopolis/configuration/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.technopolis.configuration.security.jwt.AuthTokenFilter;
+import org.technopolis.configuration.security.service.UserDetailsServiceImpl;
 
 import javax.annotation.Nonnull;
 import java.sql.Timestamp;
@@ -24,25 +26,33 @@ import java.util.Map;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
-        securedEnabled = true,
-        jsr250Enabled = true,
+        //securedEnabled = true,
+        //jsr250Enabled = true,
         prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final ObjectMapper objectMapper;
     private final AuthTokenFilter authTokenFilter;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Autowired
     public WebSecurityConfig(@Nonnull final ObjectMapper objectMapper,
-                             @Nonnull final AuthTokenFilter authTokenFilter) {
+                             @Nonnull final AuthTokenFilter authTokenFilter,
+                             @Nonnull final UserDetailsServiceImpl userDetailsService) {
         this.objectMapper = objectMapper;
         this.authTokenFilter = authTokenFilter;
+        this.userDetailsService = userDetailsService;
     }
 
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(@Nonnull final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 
     @Bean
@@ -62,17 +72,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(@Nonnull final HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.cors().and().csrf().disable().formLogin().disable().httpBasic().disable()
                 .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint()).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                    .antMatchers("/api/v1.0/user/**").hasRole("ACTIVE_USER")
-                    .anyRequest().permitAll().and();
-                /*.formLogin()
-                    .loginPage("/api/v1.0/login")
-                    .permitAll()
-                    .and()
-                .httpBasic();*/
+                    .antMatchers("/api/v1.0/**").permitAll()
+                    .antMatchers("/api/v1.0/user/**").hasRole("ACTIVE_USER");
+                    /*.antMatchers("/api/v1.0/user/**").hasRole("ACTIVE_USER")
+                    .anyRequest().permitAll();*/
 
         http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }

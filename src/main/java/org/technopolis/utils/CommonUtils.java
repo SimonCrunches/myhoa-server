22d2ebefec034ea7e910cdeb673ee87277b37a62
Makary B <boriskin.ma@edu.spbstu.ru>
2021-03-28 14:27:37 +0300

    Исправляет обрабатываемый формат принимаемой даты

--- src/main/java/org/technopolis/utils/CommonUtils.java
+++ src/main/java/org/technopolis/utils/CommonUtils.java
@@ -5,6 +5,6 @@ import java.util.Locale;
 
 public final class CommonUtils {
 
-    public static final DateTimeFormatter LOCALDATE = DateTimeFormatter.ofPattern("MMM dd, yyyy", Locale.US);
+    public static final DateTimeFormatter LOCALDATE = DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.US);
     public static final DateTimeFormatter LOCALDATETIME = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.US);
 }
